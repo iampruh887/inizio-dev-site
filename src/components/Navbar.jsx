85787c26619e8013b2e4b62717953c677169963d
Prithvi Raj <rprithvi939@gmail.com>
2025-03-03 20:31:44 +0530

    bhjasdfblajsfbjha

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./css/Nav.css";
+
+const Footer = () => {
+  return (
+    <>
+    <div className="navbar">
+          <div className="asset-x" />
+          <span className="ecell-logo">[ECELL LOGO]</span>
+          <span className="events">EVENTS</span>
+          <span className="speakers">SPEAKERS</span>
+          <span className="agenda">AGENDA</span>
+          <span className="sponsors-1">SPONSORS</span>
+          <span className="contact-us">CONTACT US</span>
+    </div>
+    </>
+  );
+};
+
+export default Footer;
