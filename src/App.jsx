23ca676f2ba2353e97b590480a17eb6f2c17ad55
iampruh887 <iampruh887@gmail.com>
2025-03-04 17:15:18 +0530

    vtsls

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sponsors from "./components/Sponsors/Sponsors";
 import Events from "./components/Events/Events";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 function App() {
   return (
-  <>
-  <Router>
-    <Navbar/>
-      <Routes>
-        {/* <Route path="/" element={<Homee />} /> */}
-        <Route path="/Events" element={<Events />} />
-        <Route path="/Sponsors" element={<Sponsors />} />
-        {/* <Route path="/Teams" element={<Teams />} /> */}
-        {/* <Route path="/Speakers" element={<Speakers />} /> */}
-        {/* <Route path="/Contact" element={<Contact />} /> */}
-      </Routes>
-  </Router>
-  {/* <Events/> */}
-  {/* <Sponsors/> */}
-  </>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Events />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Sponsors" element={<Sponsors />} />
+          {/* <Route path="/Teams" element={<Teams />} /> */}
+          {/* <Route path="/Speakers" element={<Speakers />} /> */}
+          {/* <Route path="/Contact" element={<Contact />} /> */}
+        </Routes>
+      </Router>
+      {/* <Events/> */}
+      {/* <Sponsors/> */}
+    </>
   );
 }
 
